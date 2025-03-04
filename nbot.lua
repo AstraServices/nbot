d6cf2a306db6f0cd4446643790bd7551017f4d0f
AstraServices <mehmetsiracunal1@gmail.com>
2025-03-04 17:50:33 +0300

    Update nbot.lua

--- nbot.lua
+++ nbot.lua
@@ -1,3 +1,3 @@
 game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Checking bot status...")
 wait(3)
-game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Bot status: Active - 1.0.0.0")
+game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Bot status: tive - 1.0.0.0")
